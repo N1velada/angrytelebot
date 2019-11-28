b3e1176ea270e40cfe6dbb3c6a15807675d69854
N1velada <mirny_bio@mail.ru>
2019-11-28 04:08:53 +0300

    increase

--- telegrambot.py
+++ telegrambot.py
@@ -6,17 +6,18 @@ bot  =  telebot.TeleBot('1030274384:AAGleQhVCCfv0y1xU5hVpRUugl4PZg8XDKI')
 
 current_number = 0
 
-def increase(current_number, a):
-	current_number += 1
+def increase():
+    curent_number =+ 1
+    return curent_number
 
 @bot.message_handler(func=lambda m: True)
 def echo_all_2(message: Message):
 	if 'пиздец' in message.text:
 		bot.reply_to(message, 'ты серьезно?')
-		increase(current_number, 1)
+		curent_number = increase()
 	elif 'блять' in message.text:
 		bot.reply_to(message, 'не матерись -_-')
-		increase(current_number, 1)
+		curent_number = increase()
 	elif 'сколько матов уже' in message.text:
 		bot.reply_to(message, str(current_number) + '. Живи пока.')
 		

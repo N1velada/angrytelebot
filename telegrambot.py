3b593f071c39d29f01d623563c98c9630db3001e
N1velada <mirny_bio@mail.ru>
2019-11-28 04:56:56 +0300

    test

--- telegrambot.py
+++ telegrambot.py
@@ -5,17 +5,24 @@ from inc import increase
 
 bot  =  telebot.TeleBot('1030274384:AAGleQhVCCfv0y1xU5hVpRUugl4PZg8XDKI')
 
-current_number = 0
+#current_number = 0
 
 @bot.message_handler(func=lambda m: True)
-def echo_all_2(message: Message):
-	if 'пиздец' in message.text:
+def echo(message: Message):
+	if 'пиздец' in message.text.lower():
 		bot.reply_to(message, 'ты серьезно?')
-		curent_number = increase(curent_number)
-	elif 'блять' in message.text:
+#		curent_number = increase(curent_number)
+	elif 'блять' in message.text.lower():
 		bot.reply_to(message, 'не матерись -_-')
-		curent_number = increase(curent_number)
-	elif 'сколько матов уже' in message.text:
-		bot.reply_to(message, str(current_number) + '. Живи пока.')
+	elif 'привет' in message.text.lower():
+		bot.reply_to(message, 'Привет! Как твои дела?)')
+		bot.send_message(message, 'Снова опаздывать будешь?')
+#		curent_number = increase(curent_number)
+#	elif 'сколько матов уже' in message.text:
+#		bot.reply_to(message, str(current_number) + '. Живи пока.')
+
+@bot.message_handler(content_types=['new_chat_members'])
+def greeting(message):
+    bot.reply_to(message, text='Hey =)')
 		
 bot.polling(none_stop = True)

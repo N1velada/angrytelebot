9c108bdc03a6b0d52d127d2450df3c46829b330d
N1velada <mirny_bio@mail.ru>
2019-11-28 13:21:23 +0300

    1

--- telegrambot.py
+++ telegrambot.py
@@ -5,9 +5,8 @@ from inc import increase
 
 bot  =  telebot.TeleBot('1030274384:AAGleQhVCCfv0y1xU5hVpRUugl4PZg8XDKI')
 
-#current_number = 0
-
 phrases = ['сука', 'лол']
+swearing = {'count': 0}
 
 @bot.message_handler(func=lambda m: True)
 def echo(message: Message):
@@ -16,16 +15,19 @@ def echo(message: Message):
 			bot.send_message(-303816860, 'Это слово есть в списке фраз')
 
 	if 'пиздец' in message.text.lower():
-		bot.reply_to(message, 'ты серьезно?')
-#		curent_number = increase(curent_number)
+		bot.reply_to(message, 'как это грубо.')
+		swearing['count'] += 1
 	elif 'блять' in message.text.lower():
 		bot.reply_to(message, 'не матерись -_-')
+		swearing['count'] += 1
+	elif 'писос' in message.text.lower():
+		bot.reply_to(message, 'мягко, но все равно мало приятного')
+		swearing['count'] += 1
 	elif 'привет' in message.text.lower():
 		bot.reply_to(message, 'Привет! Как твои дела?)')
 		bot.send_message(-303816860, 'Снова опаздываешь...')
-#		curent_number = increase(curent_number)
-#	elif 'сколько матов уже' in message.text:
-#		bot.reply_to(message, str(current_number) + '. Живи пока.')
+	elif 'сколько матов' in message.text.lower():
+		bot.reply_to(message, 'На данный момент матов - ' + swearing['count'])
 
 @bot.message_handler(content_types=['new_chat_members'])
 def greeting(message):

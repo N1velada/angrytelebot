3058659d8c2b5a3d69565feb38f0577e0e51093e
N1velada <mirny_bio@mail.ru>
2019-11-28 04:38:00 +0300

    increase_deploy

--- telegrambot.py
+++ telegrambot.py
@@ -1,24 +1,21 @@
 import telebot
 from telebot.types import Message
 from telebot import types
+from inc import increase
 
 bot  =  telebot.TeleBot('1030274384:AAGleQhVCCfv0y1xU5hVpRUugl4PZg8XDKI')
 
-#current_number = 0
-
-#def increase(x):
-#    x += 1
-#    return x
+current_number = 0
 
 @bot.message_handler(func=lambda m: True)
 def echo_all_2(message: Message):
 	if 'пиздец' in message.text:
 		bot.reply_to(message, 'ты серьезно?')
-#		curent_number = increase(curent_number)
+		curent_number = increase(curent_number)
 	elif 'блять' in message.text:
 		bot.reply_to(message, 'не матерись -_-')
-#		curent_number = increase(curent_number)
-#	elif 'сколько матов уже' in message.text:
-#		bot.reply_to(message, str(current_number) + '. Живи пока.')
+		curent_number = increase(curent_number)
+	elif 'сколько матов уже' in message.text:
+		bot.reply_to(message, str(current_number) + '. Живи пока.')
 		
 bot.polling(none_stop = True)

8f3027560b790bb1051dd12aaba9172cbd09ab70
N1velada <mirny_bio@mail.ru>
2019-11-28 18:07:02 +0300

    final username

--- telegrambot.py
+++ telegrambot.py
@@ -32,6 +32,6 @@ def echo(message: Message):
 @bot.message_handler(content_types=['new_chat_members'])		
 def handler_new_member(message):
     user_name = message.new_chat_member.username
-    bot.send_message(-303816860, 'Добро пожаловать, !' + user_name)
+    bot.send_message(-303816860, 'Добро пожаловать, @' + user_name + '!')
 		
 bot.polling(none_stop = True)
